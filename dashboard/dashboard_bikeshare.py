78d5ec4820a55bd10690c6c1ae5297c21d71a792
fikrionii <100980386+fikrionii@users.noreply.github.com>
2023-10-15 14:01:50 +0700

    Update dashboard_bikeshare.py

--- dashboard/dashboard_bikeshare.py
+++ dashboard/dashboard_bikeshare.py
@@ -119,7 +119,16 @@ with st.sidebar:
         value=[min_date, max_date]
     )
 
-    st.sidebar.markdown("[![LinkedIn]('https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Logo.svg.original.svg')]('https://www.linkedin.com/in/alfikri-ramadhan/')")
+st.sidebar.header("Visit my Profile:")
+
+st.sidebar.markdown("Alfikri Ramadhan")
+
+col1, col2 = st.sidebar.columns(2)
+
+with col1:
+    st.markdown("[![LinkedIn](https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg)](https://www.linkedin.com/in/alfikri-ramadhan/)")
+with col2:
+    st.markdown("[![Github](https://img.icons8.com/glyph-neue/64/FFFFFF/github.png)](https://github.com/fikrionii)")
 
 # hubungkan filter dengan main_df
 
